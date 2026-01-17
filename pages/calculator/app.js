e9c7e1555140af956700754e6e4285056319a29b
sharjeelahmed617458-commits <sharjeelahmed617458@gmail.com>
2026-01-17 07:38:55 +0500

    asighment add

---
+++ pages/calculator/app.js
@@ -0,0 +1,55 @@
+var expression = "";
+
+var exp = document.getElementById("exp");
+var result = document.getElementById("result");
+
+function calculator(value) {
+
+    
+    if (value === 'C') {
+      
+         expression = "";
+        exp.innerText = "";
+        result.innerText = "0";
+      
+        return;
+    
+    
+}
+
+
+    
+    if (value === 'DEL') {
+        expression = expression.slice(0, -1);
+        exp.innerText = expression;
+        return;
+    }
+
+    if (value === '=') {
+        if(expression === "") {
+            result.innerText = "0";
+            return;
+        }
+
+        var finalExp = "";
+        for (var i = 0; i < expression.length; i++) {
+            if (expression[i] === 'x') {
+                finalExp += '*';
+            } else {
+                finalExp += expression[i];
+            }
+        }
+
+        var answer = eval(finalExp);
+        result.innerText = answer;
+        return;
+    }
+
+  
+    expression += value;
+    exp.innerText = expression;
+    return;
+}
+
+
+
